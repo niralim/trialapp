81b0ca0fdf2d787881be91030650305e063a772c
nirali modi <nmodi_007@yahoo.com>
2015-03-10 15:06:26 -0700

    Initial Commit

---
+++ thisistrialapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  thisistrialapp
+//
+//  Created by Modi on 3/10/15.
+//  Copyright (c) 2015 Modi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ thisistrialapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  thisistrialapp
+//
+//  Created by Modi on 3/10/15.
+//  Copyright (c) 2015 Modi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
